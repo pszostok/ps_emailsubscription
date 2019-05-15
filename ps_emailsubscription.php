e18d848458d9ea991b90304317ce7e3386e94681
Codencode <info@codencode.it>
2019-05-15 09:30:08 +0200

    Update ps_emailsubscription.php

--- ps_emailsubscription.php
+++ ps_emailsubscription.php
@@ -57,7 +57,7 @@ class Ps_Emailsubscription extends Module implements WidgetInterface
 
         $this->entity_manager = $entity_manager;
 
-        $this->version = '2.3.2';
+        $this->version = '2.4.0';
         $this->author = 'PrestaShop';
         $this->error = false;
         $this->valid = false;
